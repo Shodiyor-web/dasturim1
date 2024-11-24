5a73fb51e0047ad5e35bc48a27d96c25976478e0
Shodiyor-web <shodiyorqosimbekov237@gmail.com>
2024-11-24 20:29:09 +0500

    siuu
    siuu

---
+++ project1/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+
+        <nav>
+          <h1>My Articles</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ project1/src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae nulla magnam, vero animi blanditiis, fugit atque assumenda voluptate laudantium quisquam corrupti, nihil quaerat ipsum iusto similique quam officia mollitia qui. Esse reiciendis eius dolore quos at commodi, eaque accusantium recusandae ipsa iusto totam a consequuntur saepe. Vel blanditiis reiciendis dolorem?</p>
+    </div>
+  )
+}
+
+export default About
